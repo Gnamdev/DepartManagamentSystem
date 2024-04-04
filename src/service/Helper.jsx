53ff3968cd8997f1141e210a44447ef380db99fc
Gnamdev <goutamnamdev8120@gmail.com>
2024-04-04 15:11:47 +0530

    domain name url

--- src/service/Helper.jsx
+++ src/service/Helper.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:9595";
+// export const BASE_URL = "http://localhost:9595";
+export const BASE_URL =
+  "https://departmanagamentsystem-server-production.up.railway.app";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
